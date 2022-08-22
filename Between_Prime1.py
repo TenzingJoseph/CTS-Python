bf93a64607f4d07bb59283e6f1cb33ff5be275b8
Tenzing J <84193421+TenzingJoseph@users.noreply.github.com>
2022-08-22 12:22:09 +0530

    Prime in range script

---
+++ Between_Prime1.py
@@ -0,0 +1,12 @@
+
+def between_prime(upp,low=0):
+  print('The prime numbers between {} and {} are: '.format(low,upp), end=' ')
+  for i in range(low, upp+1):
+    if i > 1:
+      for j in range(2, i):
+        if (i % j) == 0:
+          break
+      else:
+        print(i, end=" ")
+
+between_prime(25)
